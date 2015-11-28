be678cdf6b234f6e18d32fd72bcfb4faf55f64b0
Petr Žufan <petr.zufan@email.cz>
2015-11-28 04:42:06 +0100

    Update 3ac.h

--- 3ac.h
+++ 3ac.h
@@ -1,3 +1,3 @@
 
-void genInstr(int InstType, void *src1, void *src2, void *dest);
-void GenNewVariable(str *item);
+void genInstr(int InstType, void *src1, void *src2, void *dest, TinstList *list);
+void GenNewVariable(string *item);
