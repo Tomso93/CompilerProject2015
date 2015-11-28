77b2c764d298adab5449baa25b1c951d54f063b3
Petr Žufan <petr.zufan@email.cz>
2015-11-28 01:38:49 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -52,14 +52,14 @@ void printerror(int errornumber){
       break;
 
     case INTERN_ERROR:
-      fprintf(stderr, "Error %d: intern error", errornumber);
+      fprintf(stderr, "Error %d: intern error. You are not a dumbass. Authors are. Have a nice day.", errornumber);
       break;
 
     case SUCCESS:
       break;
 
     default:
-      fprintf(stderr, "Error %d: intern error", errornumber);
+      fprintf(stderr, "Error %d: intern error. You are not a dumbass. Authors are. Have a nice day.", errornumber);
       break;
   }
 }
