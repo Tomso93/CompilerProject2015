e19df91970d81b77ee0a11490edee74cd1315750
Petr Žufan <petr.zufan@email.cz>
2015-11-28 01:40:31 +0100

    Create ial.c

---
+++ ial.c
@@ -0,0 +1,15 @@
+
+#include "str.h"
+#include "ial.h"
+
+string sort(string s){
+  return s;
+}
+
+int find(string s1, string s2){
+  return 0;
+}
+
+string concat(string s1, string s2){
+  return s1;
+}
