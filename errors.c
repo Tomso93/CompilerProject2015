9cba3400b8a65a18739e95f0bd3a5eb0fa5e785d
Petr Žufan <petr.zufan@email.cz>
2015-12-02 01:11:13 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -12,54 +12,54 @@
 void printerror(int errornumber){
   switch(errornumber){
     case LEXICAL_ERROR:
-      fprintf(stderr, "Error %d: lexical error", errornumber);
+      fprintf(stderr, "Error %d: lexical error\n", errornumber);
       break;
 
     case SYNTAX_ERROR:
-      fprintf(stderr, "Error %d: syntax error", errornumber);
+      fprintf(stderr, "Error %d: syntax error\n", errornumber);
       break;
 
     case DEFINE_ERROR:
-      fprintf(stderr, "Error %d: undefine function or variable or redefining function", errornumber);
+      fprintf(stderr, "Error %d: undefine function or variable or redefining function\n", errornumber);
       break;
 
     case TYPE_ERROR:
-      fprintf(stderr, "Error %d: type mismatch or wrong count of parametrs in function", errornumber);
+      fprintf(stderr, "Error %d: type mismatch or wrong count of parametrs in function\n", errornumber);
       break;
 
     case CONVERT_ERROR:
-      fprintf(stderr, "Error %d: converting variable type error", errornumber);
+      fprintf(stderr, "Error %d: converting variable type error\n", errornumber);
       break;
 
     case SEMANTIC_ERROR:
-      fprintf(stderr, "Error %d: semantic error", errornumber);
+      fprintf(stderr, "Error %d: semantic error\n", errornumber);
       break;
 
     case READ_NUM_ERROR:
-      fprintf(stderr, "Error %d: reading number error ", errornumber);
+      fprintf(stderr, "Error %d: reading number error\n", errornumber);
       break;
 
     case UNINIT_ERROR:
-      fprintf(stderr, "Error %d: uninitialized variable", errornumber);
+      fprintf(stderr, "Error %d: uninitialized variable\n", errornumber);
       break;
 
     case ZERO_DIV_ERROR:
-      fprintf(stderr, "Error %d: devide by zero", errornumber);
+      fprintf(stderr, "Error %d: devide by zero\n", errornumber);
       break;
 
     case RUNTIME_ERROR:
-      fprintf(stderr, "Error %d: runtime error", errornumber);
+      fprintf(stderr, "Error %d: runtime error\n", errornumber);
       break;
 
     case INTERN_ERROR:
-      fprintf(stderr, "Error %d: intern error. You are not a dumbass. Authors are. Have a nice day.", errornumber);
+      fprintf(stderr, "Error %d: intern error\n", errornumber);
       break;
 
     case SUCCESS:
       break;
 
     default:
-      fprintf(stderr, "Error %d: intern error. You are not a dumbass. Authors are. Have a nice day.", errornumber);
+      fprintf(stderr, "Error %d: unknown error\n", errornumber);
       break;
   }
 }
