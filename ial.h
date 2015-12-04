6c9d19a87d7b30c03108d4d7c4a71a02ff7342e4
Petr Žufan <petr.zufan@email.cz>
2015-12-04 19:59:14 +0100

    Update ial.h

--- ial.h
+++ ial.h
@@ -6,6 +6,6 @@
  */
 
 int find(string *s, string *str);
-void FailCreate(char *s, int delka, int fail[]);
+//void FailCreate(char *s, int delka, int fail[]);
 int sort(string *str0, string *str1);
-void siftdown(char *str1, int left, int right)
+//void siftdown(char *str1, int left, int right)
