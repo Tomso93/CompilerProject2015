b34db1b3d142e39c8326343058f3df80a8039299
Petr Žufan <petr.zufan@email.cz>
2015-12-11 11:27:25 +0100

    Update 3ac.h

--- 3ac.h
+++ 3ac.h
@@ -1,4 +1,4 @@
 
-void genInstr(int InstType, tData *src1, tData *src2, tData *dest, TinstList *list);
+Tinst genInstr(int InstType, tData *src1, tData *src2, tData *dest);
 void GenNewVariable(string *item);
 tData *ReadNameVar(TinstList *list);
