869fe202542ac8fd6ca0a56b4c41456a7b8c6147
Petr Žufan <petr.zufan@email.cz>
2015-12-11 02:52:10 +0100

    Update stable_new.h

--- stable_new.h
+++ stable_new.h
@@ -22,7 +22,7 @@ typedef struct LtableItem
   struct LtableItem *nextItem;  // ukazatel na dalsi prvek tabulky symbolu
 } tLTableItem;
 
-struct LtableItem *localTS[20];
+typedef tLTableItem *localTS[TABLE_SIZE];
 
 
 //globalni tabulka symbolu
@@ -31,19 +31,19 @@ typedef struct
 {
   int funcType;
   bool isdef;
-  tLTableItem *params[20];
+  string *params[TABLE_SIZE];
   localTS *LTable;
   TinstList *LInstr;
 } tGData;
 
 typedef struct GtableItem
 {
-  string key;                  // klic, podle ktereho se bude vyhledavat = nazev identifikatoru
+  string key;  // klic, podle ktereho se bude vyhledavat = nazev identifikatoru
   tGData data;                  // data, ktera jsou ke klici pridruzena
   struct GtableItem *nextItem;  // ukazatel na dalsi prvek tabulky symbolu
 } tGTableItem;
 
-struct GtableItem *globalTS[20];
+typedef tGTableItem *globalTS[TABLE_SIZE];
 
 
 //hashovaci funkce
@@ -54,21 +54,37 @@ int hash(string *key);
 //funkce vraci int: 0 = uspech, 1-99 = neuspech (cislo chyby)
 //funkce vraci ukazatel: platny ukazatel = uspech, NULL = neuspech 
 
+
 //globalni
 int GtableInit (globalTS *T);
 int GtableFree (globalTS *T);
-int GtableInsert (globalTS *T, string *key, int funcType);
-tGData* GtableSearch (globalTS *T, string key);
-int GtableItemDelete (globalTS *T, string *key);
-tGData* GtableRead (globalTS *T, string key);
-//int GtableInsertValue (globalTS *T, string *key, Tvalue v);
+
+//vlozi funkci bez parametru, promennych a instrukci. (isdef == false)
+//Pro vlozeni parametru pouzij funkci GtableInsertParam. 
+//Pro vlozeni promenne pouzij funkci GtableInsertVar.
+//Pro vlozeni hodnoty do promenne pouzij funkci GtableInsertVarVal.
+//Pro vlozeni instrukce pouzij funkci GtableinsertInstr. 
+int GtableInsert (globalTS *T, string *key, int funcType); 
+
+tGData* GtableSearch (globalTS *T, string *key);
+int GtableInsertParam (globalTS *T, string *funcID, string *parID, int parType);
+int GtableInsertVar (globalTS *T, string *funcID, string *varID, int varType);
+int GtableInsertVarVal (globalTS *T, string *funcID, string *varID, Tvalue v);
+int GtableInsertInstr (globalTS *T, string *funcID, Tinst instrukce);
+//int GtableItemDelete (globalTS *T, string *key); jestli bude potreba, dodelam
+//tGData* GtableRead (globalTS *T, string key); jestli bude potreba, dodelam
+
+
 
 //lokalni
 int LtableInit (localTS *T);
 int LtableFree (localTS *T);
-int LtableInsert (localTS *T, string *key, int varType);
-tLData* LtableSearch (localTS *T, string key);
-int LtableItemDelete (localTS *T, string *key);
-tLData* LtableRead (localTS *T, string key);
-int LtableInsertValue (localTS *T, string *key, Tvalue v);
 
+//vlozi promennou bez hodnoty (isinit = false).
+//Pro nasledne vlozeni hodnoty pouzij LtableInsertValue.
+int LtableInsert (localTS *T, string *key, int varType); 
+
+tLData* LtableSearch (localTS *T, string *key);
+int LtableInsertValue (localTS *T, string *key, Tvalue v);
+//int LtableItemDelete (localTS *T, string *key); jestli bude potreba, dodelam
+//tLData* LtableRead (localTS *T, string key); jestli bude potreba, dodelam
