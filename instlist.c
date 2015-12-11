921c57e5d2292a0d4ba801893503ac8b1341a58a
xwojac <Wojok@seznma.cz>
2015-12-12 00:13:25 +0100

    Update instlist.c

--- instlist.c
+++ instlist.c
@@ -5,12 +5,18 @@
  *	
  */
 
-#include <stdbool.h>
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "define.h"
 #include "str.h"
+#include "errors.h"
 #include "instlist.h"
+#include "stable.h"
+#include "lex.h"
+#include "varframe.h"
+#include "interpret.h"
+#include "parser.h"
 
 
 int ListInit (TinstList *L){
