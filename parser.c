6a9d6438ded97e4c972bbe77d1c573ade47ee475
xwojac <Wojok@seznma.cz>
2015-12-11 22:20:23 +0100

    Update parser.c

--- parser.c
+++ parser.c
@@ -733,8 +733,8 @@ int _return(globalTS *global_table, string *id){
 
 	if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
 	if(token !=TOK_SEMICOLON) return SYNTAX_ERROR;
-	
-	string *LastVar = ReadNameVar(global_table.data->LInstr); // funkce na cteni nazvu posledni instrukce
+	tGData* prom = GtableSearch(global_table, id);
+	string *LastVar = ReadNameVar(prom->data->LInstr); // funkce na cteni nazvu posledni instrukce
 	Tinst *instrukce = genInstr(IRET, LastVar, NULL, NULL);
     	GtableInsertInstr(global_table, id, instrukce);
 	//return je dobre zapsan, neni co resit
