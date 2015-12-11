7692b548729754cd76d9bf4754e74be579a1d937
xwojac <Wojok@seznma.cz>
2015-12-11 23:16:37 +0100

    Update 3ac.c

--- 3ac.c
+++ 3ac.c
@@ -16,7 +16,7 @@
 int cnt = 1; // counter pro poÄŤĂ­tĂˇnĂ­ hodnot generovanĂ˝ch promÄ›nnĂ˝ch
 
 // Funkce na generovĂˇnĂ­ novĂ© instrukce, kterĂˇ je nĂˇslednÄ› vloĹľena do seznamu instrukcĂ­
-Tinst *genInstr(int InstType, string *src1, string *src2, string *dest)
+Tinst *genInstr(int InstType, string src1, string src2, string dest)
 {
     Tinst TItem;
     TItem.itype = InstType;
