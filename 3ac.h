49421a67a065044df92fa8b2bf6a4444ecb96f2e
xwojac <Wojok@seznma.cz>
2015-12-11 20:59:20 +0100

    Update 3ac.h

--- 3ac.h
+++ 3ac.h
@@ -1,4 +1,4 @@
 
 Tinst *genInstr(int InstType, string *src1, string *src2, string *dest);
 void GenNewVariable(string *item);
-tData *ReadNameVar(TinstList *list);
+string *ReadNameVar(TinstList *list);
