867439bc4738b2d32178142587d40e40db6db1bf
Petr Žufan <petr.zufan@email.cz>
2015-12-13 17:44:12 +0100

    Update 3ac.h

--- 3ac.h
+++ 3ac.h
@@ -2,3 +2,5 @@
 Tinst *genInstr(int InstType, string *src1, string *src2, string *dest);
 void GenNewVariable(string *item);
 string *ReadNameVar(TinstList *list);
+
+string *GtableLastDest(globalTS *T, string *id);
